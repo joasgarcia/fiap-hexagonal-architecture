cd4f77eb388376e37af2d35583e18b2b95d24e05
richard <richarmateus@gmail.com>
2023-06-28 21:52:04 -0300

    Ajuste script de inicialização

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into customer (id, cpf, name) values (1, '00000000001', 'Galeno de Melo')
-insert into customer (id, cpf, name) values (2, '00000000002', 'Joás Garcia')
+insert into customer (id, cpf, name) values (2, '00000000002', 'Joas Garcia')
 insert into customer (id, cpf, name) values (3, '00000000003', 'Richard Altmayer')
