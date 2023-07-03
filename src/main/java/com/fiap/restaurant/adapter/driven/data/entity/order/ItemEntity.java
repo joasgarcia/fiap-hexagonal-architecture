a3dbcffae9f2c878c37b2c1c0d10539e80e34916
Galeno de Melo <galenodemelo@gmail.com>
2023-07-03 18:45:15 -0300

    refactor: Deixa de usar `*` na importação

--- src/main/java/com/fiap/restaurant/adapter/driven/data/entity/order/ItemEntity.java
+++ src/main/java/com/fiap/restaurant/adapter/driven/data/entity/order/ItemEntity.java
@@ -1,6 +1,10 @@
 package com.fiap.restaurant.adapter.driven.data.entity.order;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity(name = "item")
 public class ItemEntity {
