075c679261b678e3527b5a4397fedea94d16664d
Galeno de Melo <galenodemelo@gmail.com>
2023-07-09 11:49:57 -0300

    refactor: Referencia interface e não a classe concreta

--- src/main/java/com/fiap/restaurant/core/service/order/OrderItemService.java
+++ src/main/java/com/fiap/restaurant/core/service/order/OrderItemService.java
@@ -1,15 +1,14 @@
 package com.fiap.restaurant.core.service.order;
 
 import com.fiap.restaurant.adapter.driven.data.repository.order.OrderItemRepository;
-import com.fiap.restaurant.core.model.order.Item;
-import com.fiap.restaurant.core.model.order.Order;
 import com.fiap.restaurant.core.model.order.OrderItem;
+import com.fiap.restaurant.core.repository.order.IOrderItemRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class OrderItemService {
 
-    private final OrderItemRepository orderItemRepository;
+    private final IOrderItemRepository orderItemRepository;
 
     public OrderItemService(OrderItemRepository orderItemRepository) {
         this.orderItemRepository = orderItemRepository;
