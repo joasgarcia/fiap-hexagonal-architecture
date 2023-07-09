72e5d921c96b5c444d101cdfe49ceb69604949b6
Galeno de Melo <galenodemelo@gmail.com>
2023-07-09 13:14:38 -0300

    refactor: Move o login para a controller de pedido

--- src/main/java/com/fiap/restaurant/adapter/driver/api/controller/order/OrderController.java
+++ src/main/java/com/fiap/restaurant/adapter/driver/api/controller/order/OrderController.java
@@ -1,16 +1,24 @@
 package com.fiap.restaurant.adapter.driver.api.controller.order;
 
+import com.fiap.restaurant.adapter.driver.api.controller.order.dto.OrderAddItemDTO;
+import com.fiap.restaurant.core.model.customer.Customer;
+import com.fiap.restaurant.core.model.order.Item;
 import com.fiap.restaurant.core.model.order.Order;
+import com.fiap.restaurant.core.service.customer.CustomerService;
 import com.fiap.restaurant.core.service.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private CustomerService customerService;
 
     @Autowired
     private OrderService orderService;
@@ -18,6 +26,15 @@ public class OrderController {
     @GetMapping("/")
     public List<Order> list() {
         return orderService.list();
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<Order> start(@RequestBody Optional<Customer> customer) {
+        Customer loggedCustomer = null;
+        if (customer.isPresent()) loggedCustomer = customerService.findOrCreate(customer.get());
+
+        Order order = this.orderService.save(loggedCustomer);
+        return ResponseEntity.ok(order);
     }
 
     @PostMapping("/{id}/item")
