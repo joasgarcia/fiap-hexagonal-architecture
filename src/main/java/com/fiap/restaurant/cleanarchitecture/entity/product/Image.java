ae7c41761f023a324c5ddc3d2117d093b4c16546
richard <richarmateus@gmail.com>
2023-08-29 22:59:10 -0300

    Mapeamento da entidade Image.

---
+++ src/main/java/com/fiap/restaurant/cleanarchitecture/entity/product/Image.java
@@ -0,0 +1,32 @@
+package com.fiap.restaurant.cleanarchitecture.entity.product;
+
+public class Image {
+
+    private Long id;
+    private String src;
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
