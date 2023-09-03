ee4f440d1e3f753cc41191f67cf70e22e0ef17cc
Joás Garcia <joasdemelo@hotmail.com>
2023-09-03 18:40:12 -0300

    Criando endpoint do webhook do gateway de pagamento para atualizar o status de pagamento do pedido. Atualizei também a collection do Postman com esse novo endpoint

--- src/main/java/com/fiap/restaurant/cleanarchitecture/usecase/order/OrderUseCase.java
+++ src/main/java/com/fiap/restaurant/cleanarchitecture/usecase/order/OrderUseCase.java
@@ -1,6 +1,7 @@
 package com.fiap.restaurant.cleanarchitecture.usecase.order;
 
 import com.fiap.restaurant.cleanarchitecture.entity.order.Order;
+import com.fiap.restaurant.cleanarchitecture.entity.order.OrderPaymentStatus;
 import com.fiap.restaurant.cleanarchitecture.gateway.order.IOrderGateway;
 import com.fiap.restaurant.cleanarchitecture.types.exception.ResourceNotFoundException;
 
@@ -10,6 +11,16 @@ public class OrderUseCase {
         Order order = orderGateway.getById(id);
 
         if (order == null) throw new ResourceNotFoundException("Pedido não encontrado");
+
+        return order;
+    }
+
+    public static Order updatePaymentStatus(Long id, OrderPaymentStatus paymentStatus, IOrderGateway orderGateway) {
+        Order order = orderGateway.getById(id);
+
+        order.setPaymentStatus(paymentStatus);
+
+        orderGateway.update(order);
 
         return order;
     }
