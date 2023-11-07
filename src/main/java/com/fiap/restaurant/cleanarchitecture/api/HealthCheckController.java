2fbb6d4ea93a73af39d17f00d452646c0e26b67e
Joás Garcia <joasdemelo@hotmail.com>
2023-11-06 22:35:23 -0300

    Criando endpoint do Health Check para o Target Group

---
+++ src/main/java/com/fiap/restaurant/cleanarchitecture/api/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.fiap.restaurant.cleanarchitecture.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> index() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("success", "true");
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+}
