859e6617f9a31ecd5c5bfb91757e3c77b3605e72
Galeno de Melo <galenodemelo@gmail.com>
2024-01-14 18:42:58 -0300

    test: Operacões com banco de dados envolvendo Customer

---
+++ src/test/java/com/fiap/restaurant/external/db/customer/CustomerJpaRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.fiap.restaurant.external.db.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
+public class CustomerJpaRepositoryTest {
+
+    @Autowired
+    private CustomerJpaRepository customerJpaRepository;
+
+    private CustomerJpa customerJpa;
+
+    @BeforeEach
+    void setup() {
+        customerJpa = new CustomerJpa();
+        customerJpa.setName("John Doe");
+        customerJpa.setEmail("johndoe@email.com");
+        customerJpa.setCpf(UUID.randomUUID().toString());
+    }
+
+    @Test
+    void mustSaveCustomer() {
+        CustomerJpa savedCustomer = customerJpaRepository.save(customerJpa);
+        assertThat(savedCustomer).isEqualTo(customerJpa);
+    }
+
+    @Test
+    void mustFindCustomerByCpf() {
+        CustomerJpa savedCustomer = customerJpaRepository.save(customerJpa);
+        CustomerJpa foundCustomer = customerJpaRepository.findByCpf(customerJpa.getCpf());
+        assertThat(foundCustomer.getCpf()).isNotEmpty().isEqualTo(savedCustomer.getCpf());
+    }
+
+    @Test
+    void mustNotFindCustomerByCpf() {
+        CustomerJpa customer = customerJpaRepository.findByCpf(customerJpa.getCpf());
+        assertThat(customer).isNull();
+    }
+}
