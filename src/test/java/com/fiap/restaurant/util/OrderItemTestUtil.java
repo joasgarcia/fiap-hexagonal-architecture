7e525713e5d6759856f8e78b317699feb3630d81
richard <richarmateus@gmail.com>
2024-01-27 10:33:41 -0300

    Métodos utilitários para geração de DTO para testes.

--- src/test/java/com/fiap/restaurant/util/OrderItemTestUtil.java
+++ src/test/java/com/fiap/restaurant/util/OrderItemTestUtil.java
@@ -3,6 +3,7 @@ package com.fiap.restaurant.util;
 import com.fiap.restaurant.external.db.order.ItemJpa;
 import com.fiap.restaurant.external.db.order.OrderItemJpa;
 import com.fiap.restaurant.external.db.order.OrderJpa;
+import com.fiap.restaurant.types.dto.order.OrderItemDTO;
 
 public class OrderItemTestUtil {
 
@@ -13,5 +14,13 @@ public class OrderItemTestUtil {
         orderItemJpa.setObservation(observation);
 
         return orderItemJpa;
+    }
+
+    public static OrderItemDTO generateDTO(Long itemId, String observation) {
+        OrderItemDTO orderItemDTO = new OrderItemDTO();
+        orderItemDTO.setItemId(itemId);
+        orderItemDTO.setObservation(observation);
+
+        return orderItemDTO;
     }
 }
