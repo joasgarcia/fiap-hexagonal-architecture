74e4618f8d4bcde74337473656af5b632faaf221
richard <richarmateus@gmail.com>
2024-01-27 11:55:55 -0300

    DTO para armazenar a resposta da comunicação com serviço de pagamento.

---
+++ src/main/java/com/fiap/restaurant/types/dto/order/OrderPaymentRestResponseDTO.java
@@ -0,0 +1,15 @@
+package com.fiap.restaurant.types.dto.order;
+
+public class OrderPaymentRestResponseDTO {
+
+    private Boolean success;
+    private String message;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
