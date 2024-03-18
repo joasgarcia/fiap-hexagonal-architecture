22e59a899d5cb0dd632f7757f03e8eb8f7d80d47
Galeno de Melo <galenodemelo@gmail.com>
2024-03-18 20:50:54 -0300

    refactor: Converte em Record para facilitar serializacão

--- src/main/java/com/fiap/restaurant/types/dto/order/payment/OrderPaymentPayloadMessageBrokerDTO.java
+++ src/main/java/com/fiap/restaurant/types/dto/order/payment/OrderPaymentPayloadMessageBrokerDTO.java
@@ -3,13 +3,6 @@ package com.fiap.restaurant.types.dto.order.payment;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonSerialize
-public class OrderPaymentPayloadMessageBrokerDTO {
+public record OrderPaymentPayloadMessageBrokerDTO(Long orderId, Double value) {
 
-    private Long orderId;
-    private Double value;
-
-    public OrderPaymentPayloadMessageBrokerDTO(Long orderId, Double value) {
-        this.orderId = orderId;
-        this.value = value;
-    }
 }
